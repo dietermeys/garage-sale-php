894a6ea207c27dad96e90f41534b2d3e1ba9edcf
Dieter Meys <dietermeys@gmail.com>
2016-08-17 12:29:25 +0200

    Add distance scope and attribute

--- app/Product.php
+++ app/Product.php
@@ -17,6 +17,18 @@ class Product extends Model
     ];
 
     /**
+     * Get the user's first name.
+     *
+     * @param  string  $value
+     * @return string
+     */
+    public function getDistanceAttribute($value)
+    {
+        $user = \Auth::getUser();
+        return DistanceCalculator::calculate($user->lat, $user->lng, $this->seller->lat, $this->seller->lng);
+    }
+
+    /**
      * @return \Illuminate\Database\Eloquent\Relations\BelongsTo|Category
      */
     public function category()
@@ -30,5 +42,27 @@ class Product extends Model
     public function seller()
     {
         return $this->belongsTo(User::class, 'user_id');
+    }
+
+    /**
+     * Scope a query to only include nearby products
+     * http://stackoverflow.com/a/1006769
+     *
+     * @param $query
+     * @param $user
+     * @param int $maxDistance
+     * @return \Illuminate\Database\Query\Builder
+     */
+    public function scopeCloseBy($query, $user, $maxDistance = 15)
+    {
+        /* @var \Illuminate\Database\Query\Builder $query */
+        $query->join("users AS sellers", "sellers.id", "=", "products.user_id");
+        $query->selectRaw("( 6371 * acos( cos( radians($user->lat) )
+                * cos( radians( sellers.lat ) )
+                * cos( radians( sellers.lng ) - radians($user->lng) )
+                + sin( radians($user->lat) )
+                * sin( radians( sellers.lat ) ) ) ) AS distance ");
+        $query->having("distance", "<", $maxDistance);
+        return $query;
     }
 }
