81d31a8310a1d65ddf193cdcd429e5405509de63
Dieter Meys <dietermeys@gmail.com>
2016-08-20 14:54:34 +0200

    Improve Product detail page

--- resources/views/products/detail.blade.php
+++ resources/views/products/detail.blade.php
@@ -9,11 +9,24 @@
 
                     <div class="panel-body">
                         <ul>
-
-                                <li>{{ $product->summary }}</li>
-                                <li>{{ $product->price }}</li>
-
+                            <li>Title: {{ $product->title }}</li>
+                            <li>Summary: {{ $product->summary }}</li>
+                            <li>Seller: {{ $product->seller->name }}</li>
+                            <li>Category: {{ $product->category->name }}</li>
+                            <li>Date: {{ $product->created_at->format('d-m-Y H:i') }}</li>
+                            <li>Price: € {{ money_format('%i', $product->price) }}</li>
+                            <li>Images:</li>
+                            @foreach($product->photos->chunk(4) as $chunk)
+                                <div class="row">
+                                    @foreach($chunk as $image)
+                                        <div class="col-md-3">
+                                            <img src="/images/products/{{$image->filename}}" alt="">
+                                        </div>
+                                    @endforeach
+                                </div>
+                            @endforeach
                         </ul>
+                        @if(Auth::user()->id == $product->user_id)
                         <form class="form-horizontal" role="form" method="POST" action="{{ route('products.destroy', $product) }}"
                               name="products.store" enctype="multipart/form-data">
                             {{ csrf_field() }}
@@ -26,6 +39,19 @@
                                 </div>
                             </div>
                         </form>
+                        <form class="form-horizontal" role="form" method="POST" action="{{ route('products.edit', $product) }}"
+                              name="products.store" enctype="multipart/form-data">
+                            {{ csrf_field() }}
+                            {{ method_field('GET') }}
+                            <div class="form-group">
+                                <div class="col-md-6 col-md-offset-4">
+                                    <button type="submit" class="btn btn-warning">
+                                        <i class="fa fa-btn fa-user"></i> Edit
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                        @endif
                     </div>
                 </div>
             </div>
