cf573a2470c0326048b4371fcf8ae5006234043f
Dieter Meys <dietermeys@gmail.com>
2016-08-20 14:55:03 +0200

    Add link to detail page in Product partial file

--- resources/views/products/partial/product.blade.php
+++ resources/views/products/partial/product.blade.php
@@ -10,5 +10,17 @@
             <li>Date: {{ $product->created_at->format('d-m-Y H:i') }}</li>
             <li>Price: € {{ money_format('%i', $product->price) }}</li>
         </ul>
+        <form class="form-horizontal" role="form" method="POST" action="{{ route('products.show', $product) }}"
+              name="products.store" enctype="multipart/form-data">
+            {{ csrf_field() }}
+            {{ method_field('GET') }}
+            <div class="form-group">
+                <div class="col-md-6 col-md-offset-4">
+                    <button type="submit" class="btn btn-info">
+                        <i class="fa fa-btn fa-user"></i> More >>>
+                    </button>
+                </div>
+            </div>
+        </form>
     </div>
 </div>
