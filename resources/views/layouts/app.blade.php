bc21d70734c5bd96ee0e65c82078f2def0a79186
Dieter Meys <dietermeys@gmail.com>
2016-08-20 23:04:34 +0200

    Add link in menu to favorites page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -71,6 +71,7 @@
                             <ul class="dropdown-menu" role="menu">
                                 <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
                                 <li><a href="{{ url('/messages') }}"><i class="fa fa-btn fa-envelope"></i>Messages</a></li>
+                                <li><a href="{{ url('/products/favorites') }}"><i class="fa fa-btn fa-star"></i>Favorites</a></li>
                             </ul>
                         </li>
                     @endif
